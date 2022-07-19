208a63ea292ba3af6f1a68fbda3c98808431f326
Sherry <sherry9624715@gmail.com>
2022-07-19 14:36:18 -0600

    Make dosage adjust field work

--- src/components/OutputCard.js
+++ src/components/OutputCard.js
@@ -3,11 +3,12 @@ import { useState } from "react"
 const OutputCard = ({drug, weight}) => {
     const [dosage, setDosage] = useState(drug.dosage_floor)
     let amount = Math.round(weight*dosage/drug.density*100)/100
+
   return (
     <div className="w-full md:w-max card p-6 ring-1 ring-gray-300 rounded-lg">
         <h1 className="text-xl font-bold">{drug.name}</h1>
-        <h1 className="text-2xl font-bold underline">{amount} ml</h1>
-        <input className="w-full ring-1 ring-gray-300 bg-transparent px-4 h-10 my-3 rounded-lg placeholder:text-slate-400"type="number" />
+        <h1 className="text-sky-500 text-2xl font-bold">{amount} ml</h1>
+        <input value={dosage} onChange={(e)=> setDosage(e.target.value)} className="w-full ring-1 ring-gray-300 bg-transparent px-4 h-10 my-3 rounded-lg placeholder:text-slate-400"type="number" />
         <ul>
             <li>Dosage:{drug.dosage_floor}~{drug.dosage_ciel}</li>
             <li>Injection way:{drug.injection}</li>
